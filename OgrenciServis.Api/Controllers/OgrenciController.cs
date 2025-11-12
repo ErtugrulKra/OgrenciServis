28e7393a12f0cf887777349dc4effe03cc0f933e
ertugrulkra <ertugrulkra@gmail.com>
2025-11-13 00:54:01 +0300

    Ogrenci CRUD işlemleri tamamlandı

--- OgrenciServis.Api/Controllers/OgrenciController.cs
+++ OgrenciServis.Api/Controllers/OgrenciController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OgrenciServis.Logic.Interface;
+using OgrenciServis.Models;
 using OgrenciServis.Models.DTO;
 
 namespace OgrenciServis.Api.Controllers
@@ -17,10 +18,71 @@ namespace OgrenciServis.Api.Controllers
             this.ogrenci = ogrenci;
         }
 
+        // GET: api/Ogrenci
         [HttpGet]
-        public ActionResult<OgrenciDto> GetOgrenciler()
+        public ActionResult<IEnumerable<OgrenciDto>> GetOgrenciler()
         {
             return Ok(this.ogrenci.TumOgrencileriListele());
+        }
+
+        // GET: api/Ogrenci/5
+        [HttpGet("{id}")]
+        public ActionResult<OgrenciDto> GetOgrenci(int id)
+        {
+            var ogrenciDto = this.ogrenci.OgrenciGetirById(id);
+
+            if (ogrenciDto == null)
+            {
+                return NotFound($"Öğrenci ID {id} bulunamadı.");
+            }
+
+            return Ok(ogrenciDto);
+        }
+
+        // POST: api/Ogrenci
+        [HttpPost]
+        public ActionResult<Ogrenci> PostOgrenci([FromBody] Ogrenci ogrenci)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var yeniOgrenci = this.ogrenci.OgrenciEkle(ogrenci);
+            return CreatedAtAction(nameof(GetOgrenci), new { id = yeniOgrenci.OgrenciId }, yeniOgrenci);
+        }
+
+        // PUT: api/Ogrenci/5
+        [HttpPut("{id}")]
+        public ActionResult<Ogrenci> PutOgrenci(int id, [FromBody] Ogrenci ogrenci)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var guncellenenOgrenci = this.ogrenci.OgrenciGuncelle(id, ogrenci);
+
+            if (guncellenenOgrenci == null)
+            {
+                return NotFound($"Öğrenci ID {id} bulunamadı.");
+            }
+
+            return Ok(guncellenenOgrenci);
+        }
+
+        // DELETE: api/Ogrenci/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteOgrenci(int id)
+        {
+            var silindi = this.ogrenci.OgrenciSil(id);
+
+            if (!silindi)
+            {
+                return NotFound($"Öğrenci ID {id} bulunamadı.");
+            }
+
+            return NoContent();
         }
     }
 }
